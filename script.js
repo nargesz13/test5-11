05e63ea3b91e7a53f8e510f56f4a4e2079435fe8
nargesz13 <nargeszi13@gmail.com>
2024-11-05 19:59:20 +0330

    Update script.js-1

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let btn=document.querySelector('.btn')
+let btn=document.querySelector('.btn');
+let text=document.querySelector('.text');
 
 btn.addEventListener('click',act)
 
 function act(){
-    btn.style.backgroundColor='#E74C3C'
+    btn.style.backgroundColor='#E74C3C';
+     btn.style.displayr='block';
+    
 }
